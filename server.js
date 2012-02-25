b12e64a79793a1f4f0e8498f8a6bfb523ea18fef
V Sreekanth <sreeix@gmail.com>
2012-02-26 02:06:36 +0530

    Adding connection pool and More improvements

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var RedisProxy = require('./lib/redis_proxy');
 var config = JSON.parse(fs.readFileSync("config/config.json"));
 console.log(config);
 var redis_proxy = new RedisProxy(config);
-var clients = 0;
 
 var server = net.createServer(function (socket) {
   console.log('client connected');
@@ -15,18 +14,14 @@ var server = net.createServer(function (socket) {
 
   socket.on('data', function(data) {
     var command = data.toString('utf8');
-    // console.log( clients++ + command);
-
     redis_proxy.sendCommand(command, function(err, res) {
-      clients = clients - 1;
       if(err){
         console.log("err:"+err);
       }
-      // console.log(clients + res.toString('utf8'));
       socket.write(res.toString('utf8'));
       if(/quit/i.test(data)){
-        // console.log("xxxxxxxxxxxxxxxxxx");
         socket.end();
+        redis_proxy.quit();
       }
     });
   });
