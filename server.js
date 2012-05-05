3102bf9468006937945ffc82aa14d5a53cdfa4a8
V Sreekanth <sreeix@gmail.com>
2012-05-05 19:30:56 +0530

    Adding the redis proxy bin file that works.

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var util = require('util');
 var RedisProxy = require('./lib/redis_proxy');
 var logger = require('winston');
 
-var config = JSON.parse(fs.readFileSync("config/config.json"));
+var configFile = process.argv[2] || "config/config.json";
+logger.info('using '+ configFile + ' as configuration source');
+var config = JSON.parse(fs.readFileSync(configFile));
 var redis_proxy = new RedisProxy(config);
 logger.level = config.debug ? 'debug' : 'info';
 
@@ -29,7 +31,6 @@ var server = net.createServer(function (socket) {
       if(/quit/i.test(data)){
         logger.debug('QUIT command received closing the connection' );
         socket.end();
-        // redis_proxy.quit(id);
       }
     });
   });
